26aacdf1b008b41b32ae4df295df05da4f9ba77c
Michelle Ellis <michelle.ellis@shopify.com>
2020-06-16 12:32:55 -0400

    created_by and search

--- gamePickerServer/games/models.py
+++ gamePickerServer/games/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Game(models.Model):
   title = models.TextField()
-  
+  created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
--- gamePickerServer/games/schema.py
+++ gamePickerServer/games/schema.py
@@ -1,7 +1,10 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 
 from .models import Game
+from users.schema import UserType
+from django.db.models import Q
 
 
 class GameType(DjangoObjectType):
@@ -10,25 +13,41 @@ class GameType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    games = graphene.List(GameType)
+    games = graphene.List(GameType, search=graphene.String())
 
-    def resolve_games(self, info, **kwargs):
+    def resolve_games(self, info, search=None, **kwargs):
+        if search:
+            filter = (
+                Q(title__icontains=search)
+            )
+            return Game.objects.filter(filter)
+
         return Game.objects.all()
 
 class CreateGame(graphene.Mutation):
     id = graphene.Int()
     title = graphene.String()
+    created_by = graphene.Field(UserType)
+
 
     class Arguments:
         title = graphene.String()
 
     def mutate(self, info, title):
-        game = Game(title=title)
+        user = info.context.user or None
+        if user.is_anonymous:
+            raise GraphQLError('You must be logged to vote!')
+
+        game = Game(
+            title=title,
+            created_by=user
+        )
         game.save()
 
         return CreateGame(
             id=game.id,
             title=game.title,
+            created_by=game.posted_by,
         )
 
 class Mutation(graphene.ObjectType):
